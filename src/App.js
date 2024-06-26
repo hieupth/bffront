3e1e9c47def9fc20c7a28fbceecca53da77ab680
hieupth <64821726+hieupth@users.noreply.github.com>
2024-06-26 23:44:57 +0700

    chore: add caption

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 /* eslint-disable jsx-a11y/alt-text */
 import "./App.css";
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import UserImage from "./components/UserImage";
 
 function App() {
   return (
     <Container>
       <Row>
-        <img
-          className="slogan center"
-          src={process.env.PUBLIC_URL + "slogan.png"}
-          width="100%"
-        ></img>
+        <Col md={2} lg={2}></Col>
+        <Col xs={12} md={8} lg={8} className='text-align-center'>
+          <img className='slogan' src={process.env.PUBLIC_URL + "slogan.png"} max-width="100%" height="100px"></img>
+        </Col>
+        <Col></Col>
       </Row>
-      <div className="wrap">
+      <Row>
+        <Col md={2} lg={2}></Col>
+        <Col xs={12} md={8} lg={8} className='text-align-center'>
+          <UserImage/>
+        </Col>
+      </Row>
+      {/* <div className="wrap">
         <Row className="justify-content-center container-main">
           <UserImage />
         </Row>
-      </div>
+      </div> */}
     </Container>
   );
 }
--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useRef } from "react";
-import { Button, Image } from "react-bootstrap";
+import { Button, Image, Row, Col } from "react-bootstrap";
 import Webcam from "react-webcam";
 import ReCAPTCHA from "react-google-recaptcha";
 
 const UserImage = () => {
+  let caption = ''
+  // let step = 0
   const [image, setImage] = useState("/sky.png");
   const [file, setFile] = useState(null);
   const [imageUploaded, setImageUploaded] = useState(false);
   const [isLoading, setIsLoading] = useState(false); // Trạng thái cho biết ảnh đang được tải
   const [showWebcam, setShowWebcam] = useState(false);
-  const [showCaptcha, setShowCaptcha] = useState(false);
   const fileInputRef = useRef(null);
   const webcamRef = useRef(null);
-  const recaptchaRef = useRef(null);
+  const [step, setStep] = useState(0);
 
   const handleFileSelect = () => {
     fileInputRef.current.click();
@@ -46,18 +47,9 @@ const UserImage = () => {
       });
   };
 
-  const handleCaptcha = (value) => {
-    console.log("Captcha value:", value);
-    if (value) {
-      uploadImage();
-    } else {
-      alert("CAPTCHA verification failed. Please try again.");
-    }
-  };
-
   const handleContinue = () => {
     if (!file) {
-      alert("Vui lòng chọn ảnh trước khi tiếp tục!");
+      alert("Vui lòng chọn hoặc chụp ảnh trước khi tiếp tục!");
       return;
     }
     uploadImage();
@@ -65,15 +57,22 @@ const UserImage = () => {
   };
 
   const uploadImage = async () => {
+    setStep(1);
+
     setIsLoading(true); // Bắt đầu hiển thị ảnh loading
     // setShowCaptcha(false); // Hide CAPTCHA after successful validation
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("caption", "Your image caption");
+    // formData.append("caption", caption);
+    let uri = ''
+    if (caption.length > 0) {
+      uri = '?caption=' + caption
+    }
 
     try {
       const response = await fetch(
-        "https://api.blueforestphathoachandung.com/run-url",
+        "https://api.blueforestphathoachandung.com/run-url" + uri,
+        //"http://localhost:8000/run-url/" + uri,
         {
           method: "POST",
           body: formData,
@@ -90,15 +89,15 @@ const UserImage = () => {
       setImage(data.url);
     } catch (error) {
       console.error("Error uploading image:", error);
-      alert("Lỗi khi tải ảnh lên: " + error.message);
+      alert('Không thể nhận diện khuôn mặt. Hãy thử lại ảnh khác.');
     } finally {
       setIsLoading(false); // Dừng hiển thị ảnh loading
     }
   };
 
   return (
-    <div className="user-image">
-      <div className="image-container">
+    <>
+      <Row className="image-container">
         {/* <img src="/square.png" className="border-image" width="580px" alt="img1"/> */}
         {/* <img src="/Hoa.png" className="hoa-1" width="50px" alt="img2"/>
         <img src="/Hoa.png" className="hoa-2" width="100px" alt="img3"/> */}
@@ -113,37 +112,18 @@ const UserImage = () => {
               className="webcam-view"
             />
             <div className="text-align-center">
-            <Button onClick={capture}>
-              Chụp ảnh
-            </Button>
-            <Button variant="secondary" onClick={() => setShowWebcam(false)}>
-              Đóng Camera
-            </Button>
+              <Button onClick={capture}>Chụp ảnh</Button>
+              <Button variant="secondary" onClick={() => setShowWebcam(false)}>Đóng Camera</Button>
             </div>
           </>
         ) : (
           <>
-            <Image
-              className="img-preview"
-              src={isLoading ? "/loading_screen.png" : image}
-            />
+            <Image className="img-preview" src={isLoading ? "/loading_screen.png" : image}/>
             {!imageUploaded && !isLoading && (
               <div className="button-overlay">
-                <Button
-                  className="btn-upload-image"
-                  variant="primary"
-                  onClick={() => setShowWebcam(true)}
-                >
-                  Chụp chân dung
-                </Button>
+                <Button className="btn-upload-image" variant="primary" onClick={() => setShowWebcam(true)}>Chụp chân dung</Button>
                 <span>Hoặc</span>
-                <Button
-                  className="btn-upload-image"
-                  variant="secondary"
-                  onClick={handleFileSelect}
-                >
-                  Tải lên từ máy
-                </Button>
+                <Button className="btn-upload-image" variant="secondary" onClick={handleFileSelect}>Tải lên từ máy</Button>
               </div>
             )}
           </>
@@ -155,13 +135,31 @@ const UserImage = () => {
           onChange={handleImageChange}
           style={{ display: "none" }}
         />
-      </div>
-      <div className="main-btn-continue">
-        <Button className="btn-continue" onClick={handleContinue}>
-          Tiếp tục
-        </Button>
-      </div>
-    </div>
+      </Row>
+      <Row className="user-image-btn-area">
+        {step == 0 ? (
+          <Row>
+            <Col><button className="btn-continue" onClick={handleContinue}>Tiếp tục</button></Col>
+          </Row>
+          ) : (
+          <>
+          {isLoading ? (<></>) : (
+          <>
+          <Row>
+            <Col>
+              <input className="text-caption" type="text" onKeyDown={(e) => {
+                if (e.key == 'Enter') {caption = e.target.value; handleContinue()}
+              }}></input>
+            </Col>
+          </Row>
+          <Row>
+            <Col><button className="btn-slogan" onClick={() => {caption = ''; handleContinue()}}>Đổi thông điệp</button></Col>
+          </Row>
+          </>)}
+          </>
+        )}
+      </Row>
+    </>
   );
 };
 
